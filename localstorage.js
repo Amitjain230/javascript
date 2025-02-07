1dc0d63ae51f424eaf03a3d1ff8eb98996f39bb5
Amitjain230 <amitassignment45@gmail.com>
2025-02-07 18:57:32 +0530

    success

---
+++ localstorage.js
@@ -0,0 +1,13 @@
+//localstorage.clear is used to clear all the data.
+//localstorage.remove(key name) is used to remove any unique item.
+//key is always unique.
+//H.W(wap )
+
+
+
+localStorage.setItem("Age",);
+localStorage.setItem("name","Atul");
+let fname="manish kuch bhi"
+localStorage.setItem("fname",fname);
+
+localStorage.removeItem('name');
