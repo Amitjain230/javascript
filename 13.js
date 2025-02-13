c97bcaa01bc2e8504890fb0abd674f6010a56c00
Amitjain230 <amitassignment45@gmail.com>
2025-02-13 19:23:51 +0530

    success

---
+++ 13.js
@@ -0,0 +1,9 @@
+//node is the way in which we can run our code of javascript.
+//node js is the runtime enviroment.
+//Npm->Node package manager.
+//Npm is the package manager 
+//Json->javascript object Notataion.
+//json run on server.
+//for run the json file or extention is jason-server.
+//jb tak server on hoga jb tk run hogi file.
+//to run the file json-server --watch filename.json.
